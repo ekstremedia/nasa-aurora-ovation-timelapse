d55039a732927829e3a3786bba1eeff8f770ec52
Terje Nesthus <terje@ekstremedia.no>
2021-12-28 15:25:55 +0100

    Removed line

--- make24htimelapse.js
+++ make24htimelapse.js
@@ -29,7 +29,6 @@ let download = function(uri, filename, callback){
   request.head(uri, function(err, res, body){
     // console.log('content-type:', res.headers['content-type']);
     // console.log('content-length:', res.headers['content-length']);
-
     request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
   });
 };
